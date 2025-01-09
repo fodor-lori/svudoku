088e858bfc44db0d26ab37ee8fed46f32f77aa52
fodor-lori <fodor.lori@hotmail.com>
2025-01-09 22:31:48 +0200

    Added vercel analitycs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>
 
